bba2dfbe1e00a91e67117fc2d815d167256a103a
pmbpg <pmbpg@yahoo.com>
2026-04-21 17:43:21 +0330

    add 3D effect for tour cards

--- components/template/main/card.js
+++ components/template/main/card.js
@@ -7,8 +7,24 @@ import { jalalidate } from "@/core/helper/convertDate";
 import { calculateDays } from "@/core/helper/calculateDays";
 import { Button } from "@/components/ui/button";
 import { motion } from "motion/react";
+import { useEffect, useRef } from "react";
+import VanillaTilt from "vanilla-tilt";
 
 function Card({ data }) {
+  //-------------3d effect for card(vanilla tilt)-------------
+  const tiltRef = useRef(null);
+  useEffect(() => {
+    const element = tiltRef.current;
+
+    VanillaTilt.init(element, {
+      max: 15,
+      speed: 400,
+      glare: true,
+      "max-glare": 0.2,
+    });
+
+    return () => element.vanillaTilt.destroy();
+  }, []);
   const navigate = useRouter();
   const dates = jalalidate(data.startDate, data.endDate);
   const day = calculateDays(data.startDate, data.endDate);
@@ -19,14 +35,9 @@ function Card({ data }) {
       exit={{ scale: 0.5 }}
       key={data.id}
       className="overflow-hidden transform border rounded-lg bg-card"
+      ref={tiltRef}
     >
-      <Image
-        src={data.image}
-        width={300}
-        height={170}
-        alt={data.title}
-        className="transition  hover:scale-105 hover:translate-y-[-5px]"
-      />
+      <Image src={data.image} width={300} height={170} alt={data.title} />
       <p className="text-[22px] mt-2 mr-2 font-bold">{data.title}</p>
       <p className="my-3 mr-4 text-muted-foreground">{`${dates[2]} ماه. ${toPersianDigits(day)}روزه `}</p>
       <div className="flex justify-between w-full p-2 border-t">
